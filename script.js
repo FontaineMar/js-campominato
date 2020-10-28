2bd74b63a43e7e81ce1748b4887d08b08c7fa9a9
marco <odri.fo@gmail.com>
2020-10-28 19:32:52 +0100

    ancora in alto mare

--- script.js
+++ script.js
@@ -3,24 +3,38 @@ function numRandom(min, max){
 return  Math.floor(Math.random() * (max - min) + min);
 }
 
-console.log(numberComputer)
+
 
 var numberPlayer = [];
 var numberComputer = [];
-
-for (var i = 0; i < 4; i++){
-  var input =prompt("inserisci un numero?");
-  if(numberPlayer === numberPlayer.length){
-  numberPlayer.push(input);
-  console.log(numberPlayer)
-}
-}
-
-for (var i = 0; i < 4; i++){
-  numberComputer.push(numRandom(1, 101));
-  console.log(numberComputer)
+var listNumRandom;
+var maxChance = 8 ;
+var message;
+while (numberComputer.length < 8){
+  listNumRandom = numRandom(1, 20)
+     if (numberComputer.indexOf(listNumRandom) == - 1)
+     {
+     numberComputer.push(listNumRandom);
+     console.log(numberComputer)
+   }
 }
 
-if(numberComputer == numberPlayer.length){
+while (numberPlayer.length < maxChance){
+  var input =prompt("inserisci un numero")
+  numberPlayer.push(input)
 
+  if(numberComputer.includes(input)){
+    message = "hai perso"
+    console.log(message)
+  }
+
+  // else if (numberPlayer.includes(input)){
+  //     message = "numero gia inserito";
+  // }
+
+  else if ((input < 1) || (input > 100)){
+       message ="numero non corretto"
+  }
+
+  console.log(message)
 }
